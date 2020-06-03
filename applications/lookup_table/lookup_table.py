eab60201f50dbbf1f320b6d126cd43d4710f2c9b
CAM603 <cameronhawley@comcast.net>
2020-06-03 15:11:25 -0400

    add second pass of lookup table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -15,11 +15,21 @@ def slowfun_too_slow(x, y):
 cache = {}
 
 
+def lookup_table():
+    for x in range(2, 14):
+        for y in range(3, 6):
+            v = math.pow(x, y)
+            v = math.factorial(v)
+            v //= (x + y)
+            v %= 982451653
+            cache[(x, y)] = v
+
+
+lookup_table()
+
+
 def slowfun(x, y):
-    """
-    Rewrite slowfun_too_slow() in here so that the program produces the same
-    output, but completes quickly instead of taking ages to run.
-    """
+
     if (x, y) in cache:
         return cache[(x, y)]
 
@@ -29,7 +39,23 @@ def slowfun(x, y):
     v %= 982451653
 
     cache[(x, y)] = v
-    return v
+    return cache[(x, y)]
+
+# def slowfun(x, y):
+#     """
+#     Rewrite slowfun_too_slow() in here so that the program produces the same
+#     output, but completes quickly instead of taking ages to run.
+#     """
+#     if (x, y) in cache:
+#         return cache[(x, y)]
+
+#     v = math.pow(x, y)
+#     v = math.factorial(v)
+#     v //= (x + y)
+#     v %= 982451653
+
+#     cache[(x, y)] = v
+#     return v
 
 
 # Do not modify below this line!
